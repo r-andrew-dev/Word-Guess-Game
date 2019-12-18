fbbf46ad3f1f3f554fe3ac7a6954a86513c13fa0
Rachael Andrew <r.andrew.dev@gmail.com>
2019-12-18 02:12:46 -0600

    javascript is up and running, word is displaying on page in the HTML, key selections are filling in the word, wins are going up when word is guessed correctly, completely,losses is going up if guesses remaining gets to 0, guesses remaining is going down,

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,164 @@
-// Choose a theme for your game! In the demo, we picked an 80s theme: 80s questions, 80s sound and an 80s aesthetic. You can choose any subject for your theme, though, so be creative!
 
-// Guess that OS! Android OS's
-
-// Use key events to listen for the letters that your players will type.
-
-
-
-// Display the following on the page:
-
-
-// Press any key to get started!
-
-
-// Wins: (# of times user guessed the word correctly).
-
-
-// If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
-
-
-// As the user guesses the correct letters, reveal them: m a d o _ _ a.
-
-
-
-
-// Number of Guesses Remaining: (# of guesses remaining for the user).
-
-
-// Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
-
-
-// After the user wins/loses the game should automatically choose another word and make the user play it.
-
-// 
-// 
-// 
-// 
-var directionsText = document.getElementById("directions-Text");
-var currentVersionText = document.getElementById("current-Version");
-var guessesRemainingText = document.getElementById("guesses-Remaining");
+// Sets up elements in the HTML to be dynamically changed using Javascript
+var directionsText = document.getElementById("directions-text")
+var currentVersionText = document.getElementById("current-version");
+var lettersGuessed = document.getElementById("letters-guessed");
+var guessesRemainingText = document.getElementById("guesses-remaining");
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 
 
-// var wordGuessGame = {
+// An array holding all of the Android Versions we are providing for the user to guess.
 var androidVersions = ["cupcake", "donut", "eclair", "froyo", "gingerbread", "honeycomb", "icecreamsandwich", "jellybean", "kitkat", "lollipop", "marshmallow", "nougat", "oreo", "pie"];
+
+// Allowing the computer to randomly choose the version the user will try to guess.
 var computerChooseVersion = androidVersions[Math.floor(Math.random() * androidVersions.length)];
-var versionAsDashes = computerChooseVersion.replace(/[a-zA-Z]/g , '-');
-
-currentVersionText.textContent = "Version: " + versionAsDashes;
-
-document.onkeyup = function(event) {
-
-        var userGuess = event.key
 
 
-        
+// Storing the version chosen by the computer into an array for easier comparisons later.
+var computerChooseVersionArray = computerChooseVersion.split("");
+
+// Storing the verison chosen by the computer as a string of dashes.
+var versionAsDashes = computerChooseVersion.replace(/[a-zA-Z]/g, '-');
+
+// Storing the string of dashes as an array.
+var versionAsDashesArray = versionAsDashes.split("")
 
 
-console.log(userGuess);
 
+console.log(computerChooseVersion)
+
+
+
+// Setting inital variables for game start.
+var guessesRemaining = 3;
+var lettersGuessedArray = [];
+var losses = 0;
+var wins = 0;
+
+
+// Setting message user will be paying attention to at game start.
+document.getElementById("current-game-message").textContent = "Press any Letter to Get Started!";
+
+
+// Creating an event that will be executed when the user presses any key.
+document.onkeyup = function (event) {
+
+        // Hiding the message user first saw providing directions for the game.
+        document.getElementById("current-game-message").textContent = "";
+
+        // storying the user's input ket as a variable to be utilized.
+        var userGuess = event.key.toLowerCase();
+
+       
+        // if the Android Version the computer chose for the user to guess does not contain the
+        //  letter the user guesses AND the letter has not already been guessed, this code will run
+        if (computerChooseVersionArray.includes(userGuess) == false && lettersGuessedArray.includes(userGuess) == false) {
+
+              
+                // adds the usersGuess into the lettersGuessedArray and decrements guesses remaining by one.
+                lettersGuessedArray.push(userGuess);
+                guessesRemaining--;
+
+                // if the above conditions are met and guessesRemaining = 0, this code will run.
+                if (guessesRemaining === 0) {
+                    
+                        // Adds one to losses, resets guessesRemaining to 3, resets the Letters Already Guessed,
+                        //  alerts the user they have lost and to try again.
+                        losses++;
+                        guessesRemaining = 3;
+                        lettersGuessedArray = [];
+                        document.getElementById("current-game-message").textContent = "YOU ARE OUT OF GUESSES. TRY AGAIN."
+
+
+
+                }
+
+        }
+
+
+
+
+
+
+
+        // If none of the other above conditions are met, this code will run.
+        else {
+
+                // this code iterates through the Android Version the computer has chosen, 
+                // checking if the user guessed letter is inside the Android Version the computer selected.
+                // If this is the case, the index of the matching index of the dashes array is set to userGuess, or filled in in the word.
+                for (var i = 0; i < computerChooseVersionArray.length; i++) {
+                        if (computerChooseVersionArray[i] === userGuess) {
+                                versionAsDashesArray[i] = userGuess;
+
+                                
+                                console.log(versionAsDashesArray)
+                                console.log(versionAsDashesArray.includes("-"))
+                                console.log(computerChooseVersion)
+                                console.log(androidVersions)
+
+
+                                if (versionAsDashesArray.includes("-") == false) {
+                                        wins++;
+                                        delete 
+                                       
+                                        console.log(versionAsDashesArray)
+                                        console.log(versionAsDashesArray.includes("-"))
+                                        console.log(computerChooseVersion)
+                                        console.log(androidVersions)
+                                }
+
+                        }
+
+                }
+        }
+
+
+
+
+        currentVersionText.textContent = versionAsDashesArray.join("  ")
+        guessesRemainingText.textContent = guessesRemaining;
+        lettersGuessed.innerText = lettersGuessedArray.join("  ");
+        lossesText.textContent = losses;
+        winsText.textContent = wins;
 }
 
-console.log(computerChooseVersion);
-console.log(versionAsDashes);
+
+
+
+
+
+currentVersionText.textContent = versionAsDashesArray.join("  ")
+guessesRemainingText.textContent = guessesRemaining;
+lossesText.textContent = losses;
+winsText.textContent = wins;
+
+
+
+
+
+
+
+
+
+
+//         }
+// console.log(computerChooseVersion)
+// console.log(computerChooseVersionArray)
+
+// console.log(userGuess);
+
+// console.log(computerChooseVersion.indexOf(userGuess));
+
+// console.log(computerChooseVersionArray.indexOf(userGuess));
+
+// console.log(lettersGuessed);
+// console.log(lettersGuessed.textContent); 
+
+// }
+
+// lettersGuessed.textContent = lettersGuessed;
 
 
 
@@ -78,4 +174,4 @@ console.log(versionAsDashes);
 
     //If user gets all the correct letters, guessing the movie title/string, wins goes up by one, a movie clip/image plays or is shown, song plays, and a new string is selected by the computer. 
 
-    // If guesses reaches 0, player loses and a new string is selected by the computer.
+    // If guesses reaches 0, player loses and a new string is selected by the computer
