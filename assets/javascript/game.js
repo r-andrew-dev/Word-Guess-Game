f2ef627bfeb16ed2d007ed0151fecd8bcdf44882
Rachael Andrew <r.andrew.dev@gmail.com>
2019-12-19 18:38:42 -0600

    Began styling elements using bootstrap, jumbotron added, moved current game message and button inside jumbotron.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -37,6 +37,7 @@ var wins = 0;
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "S", "t", "u", "v", "w", "x", "y", "z"]
 
 
+
 // Sets starting game message. 
 document.getElementById("current-game-message").textContent = "Click New Version to Get Started!";
 
